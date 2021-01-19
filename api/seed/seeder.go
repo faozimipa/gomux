d5983248327dc3cba1c1353601bd21bd4544fc94
Faozi <faozimipa@gmail.com>
2021-01-19 19:02:03 +0700

    refactor code

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -8,12 +8,12 @@ import (
 )
 
 var users = []models.User{
-	models.User{
+	{
 		Nickname: "Steven victor",
 		Email:    "steven@gmail.com",
 		Password: "password",
 	},
-	models.User{
+	{
 		Nickname: "Martin Luther",
 		Email:    "luther@gmail.com",
 		Password: "password",
@@ -21,11 +21,11 @@ var users = []models.User{
 }
 
 var posts = []models.Post{
-	models.Post{
+	{
 		Title:   "Title 1",
 		Content: "Hello world 1",
 	},
-	models.Post{
+	{
 		Title:   "Title 2",
 		Content: "Hello world 2",
 	},
@@ -49,7 +49,7 @@ func Load(db *gorm.DB) {
 		}
 	*/
 
-	for i, _ := range users {
+	for i := range users {
 		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
 		if err != nil {
 			log.Fatalf("cannot seed users table: %v", err)
